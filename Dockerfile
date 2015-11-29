24f7e4a22e411d0500371b775259cf8c33d11c50
Deepan Sathyamoorthy <deepanrsat@gmail.com>
2015-11-29 09:33:52 -0600

    adding simple java docker file

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM java:latest
+
+COPY JavaHelloWorld.java
+
+RUN javac JavaHelloWorld.java
+
+CMD["java", "JavaHelloWorld"]
---
+++ JavaHelloWorld.java
@@ -0,0 +1,6 @@
+public class JavaHelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello TFX Devs");
+		System.out.println("Move to Docker, because its awesome!!");	
+	}
+}
